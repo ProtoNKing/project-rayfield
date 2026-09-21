a7c1685cb2cec9a214d555fe591d49a9f895d0f4
Use-lessDev <morajoetorajo2@gmail.com>
2026-09-21 00:05:06 -0300

    Add PressurizedChromiteConveyor class

---
+++ src/rayfield/content/PressurizedChromiteConveyor.java
@@ -0,0 +1,25 @@
+package rayfield.content;
+
+import mindustry.content.*;
+import mindustry.type.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.meta.*;
+
+import static mindustry.type.ItemStack.*;
+
+public class PressurizedChromiteConveyor extends Conveyor {
+
+    public PressurizedChromiteConveyor(String name) {
+        super(name);
+
+        requirements(Category.distribution, with(
+            Items.copper, 1,
+            Items.lead, 1
+        ));
+
+        health = 55;
+        speed = 0.046f;
+        displayedSpeed = 5.5f;
+        itemCapacity = 3;
+    }
+}
