f43bbcc20d9290e156ad1805fc5f2b268dd7241e
Use-lessDev <morajoetorajo2@gmail.com>
2026-09-20 23:58:07 -0300

    Create RayFieldJavaMod class for RayField project

---
+++ src/Rayfield/RayFieldJavaMod.java
@@ -0,0 +1,20 @@
+package rayfield;
+
+import arc.util.*;
+import mindustry.mod.*;
+import rayfield.content.PressurizedChromiteConveyor;
+
+public class RayFieldJavaMod extends Mod {
+
+    public static PressurizedChromiteConveyor pressurizedChromiteConveyor;
+
+    public RayFieldJavaMod() {
+        Log.info("Loaded Project RayField");
+    }
+
+    @Override
+    public void loadContent() {
+        Log.info("Loading RayField content...");
+        pressurizedChromiteConveyor = new PressurizedChromiteConveyor("pressurized-chromite-conveyor");
+    }
+}
